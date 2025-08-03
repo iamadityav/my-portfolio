97da09ecce6da2adf57fc178af90532afb529cd0
GuestUser01 <GuestUser01@DIGIT-G-MAC053.local>
2025-08-03 20:54:42 +0530

    added:predeploy_deploy:changed_dist:changed_vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/my-portfolio/', // ✅ This is the crucial fix for GitHub Pages
   plugins: [
     react(),
     tailwindcss(),
   ],
-})
+});
