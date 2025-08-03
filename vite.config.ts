079a9c56b492d7ecb891bf85ffb7d5b3b8f17b95
GuestUser01 <GuestUser01@DIGIT-G-MAC053.local>
2025-08-03 22:20:15 +0530

    removed_git_ignore

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-import tailwindcss from '@tailwindcss/vite';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: '/my-portfolio/', // ✅ This is the crucial fix for GitHub Pages
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
-});
+  plugins: [react()],
+  base: '/my-portfolio/',
+})
